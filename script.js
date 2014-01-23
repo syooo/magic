84ebca8ddb056a75e474d78a4ba64e2fbd2d9c4e
syooo <syooo@mail.ru>
2014-01-24 02:25:34 +0400

    Some cleanup

--- script.js
+++ script.js
@@ -11,255 +11,228 @@
 // - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
-	jQuery(function() {
+    jQuery(function () {
 
-		//alert(jQuery(window).height());
+        $('body').scrollspy({target: '.navbar-scrollspy', offset: 60});
 
-		/*jQuery('#edit-field-activity .form-type-checkbox label, #edit-field-interests .form-type-checkbox label, #edit-field-occasion .form-type-checkbox label, #edit-field-relationship .form-type-radio label').hover(function(){
-				jQuery(this).animate({fontSize: "36px" }, 100);
-			}, function(){
-				jQuery(this).animate({fontSize: "26px" }, 100);
-		});*/
+        //alert(jQuery(window).height());
 
-		//jQuery("#magial-submit-ajax input:text").attr("disabled", true);
+        /*jQuery('#edit-field-activity .form-type-checkbox label, #edit-field-interests .form-type-checkbox label, #edit-field-occasion .form-type-checkbox label, #edit-field-relationship .form-type-radio label').hover(function(){
+         jQuery(this).animate({fontSize: "36px" }, 100);
+         }, function(){
+         jQuery(this).animate({fontSize: "26px" }, 100);
+         });*/
 
-
-		jQuery("#magial-submit-ajax input:checkbox").change(function(){
-			jQuery("label[for=" + jQuery(this).attr("id") + "]").toggleClass("checked");
-		});
-
-		jQuery("#edit-field-sex input:radio").change(function(){
-			jQuery("#edit-field-sex label").removeClass("checked");
-			jQuery("label[for=" + jQuery(this).attr("id") + "]").toggleClass("checked");
-		});
-
-		jQuery("#edit-field-astrological-sign input:radio").change(function(){
-			jQuery("#edit-field-astrological-sign label").removeClass("checked");
-			jQuery("label[for=" + jQuery(this).attr("id") + "]").toggleClass("checked");
-		});
-
-		jQuery("#edit-field-relationship input:radio").change(function(){
-			jQuery("#edit-field-relationship label").removeClass("checked");
-			jQuery("label[for=" + jQuery(this).attr("id") + "]").toggleClass("checked");
-			var value = jQuery(this).val();
-			jQuery("#slider-relationship").slider("value", value);
-		});
-
-		jQuery("#edit-field-sex").css("margin-top", -30 + jQuery("#edit-field-sex").height() / -2);
+        //jQuery("#magial-submit-ajax input:text").attr("disabled", true);
 
 
+        jQuery("#edit-field-relationship input:radio").change(function () {
+            var value = jQuery(this).val();
+            jQuery("#slider-relationship").slider("value", value);
+        });
+
+//        jQuery("#edit-field-sex").css("margin-top", -30 + jQuery("#edit-field-sex").height() / -2);
+//
+//
+//        jQuery(".form-item-field-age-max").css("margin-top", -30 + (jQuery(".form-item-field-age-max").height() + 20) / -1);
+//        jQuery(".form-item-field-age-min").css("margin-top", -30 + (jQuery(".form-item-field-age-min").height() + 20) / -1);
+//
+//
+//        jQuery(".form-item-field-price-max").css("margin-top", (jQuery(".form-item-field-price-max").height() + 20) / -1);
+//        jQuery(".form-item-field-price-min").css("margin-top", (jQuery(".form-item-field-price-min").height() + 20) / -1);
+//
+//
+//        jQuery("#edit-field-relationship").css("margin-top", -30 + jQuery("#edit-field-relationship").height() / -1);
+//        jQuery("#edit-field-activity").css("margin-top", -30 + jQuery("#edit-field-activity").height() / -2);
+//        jQuery("#edit-field-post").css("margin-top", -30 + jQuery("#edit-field-post").height() / -2);
+//        jQuery("#edit-field-astrological-sign").css("margin-top", -30 + jQuery("#edit-field-astrological-sign").height() / -2);
+//        jQuery("#edit-field-interests").css("margin-top", -30 + jQuery("#edit-field-interests").height() / -2);
+//        jQuery("#edit-field-occasion").css("margin-top", -30 + jQuery("#edit-field-occasion").height() / -2);
 
 
-		jQuery(".form-item-field-age-max").css("margin-top", -30 + (jQuery(".form-item-field-age-max").height() + 20) / -1);
-		jQuery(".form-item-field-age-min").css("margin-top", -30 + (jQuery(".form-item-field-age-min").height() + 20)  / -1);		
+        function resize() {
+            //alert(jQuery('#edit-field-sex').height());
+
+            var current = jQuery(window).scrollTop();
+            var total = jQuery(document).height();
+            var hPrecent = current / total;
 
 
-		jQuery(".form-item-field-price-max").css("margin-top", (jQuery(".form-item-field-price-max").height() + 20) / -1);
-		jQuery(".form-item-field-price-min").css("margin-top", (jQuery(".form-item-field-price-min").height() + 20)  / -1);
+            var h = jQuery(window).height() - 60 < 600 ? 600 : jQuery(window).height() - 60;
+            var pad = jQuery(window).height() * .20;
+            jQuery(".results").css("min-height", h);
+            jQuery("#magial-submit-ajax .form-wrapper").height(h);
+            var sex_h = h - 122;
+            jQuery("#edit-sex").height(sex_h);
 
 
+            var now = jQuery(document).height();
 
-		jQuery("#edit-field-relationship").css("margin-top", -30 + jQuery("#edit-field-relationship").height() / -1);
-		jQuery("#edit-field-activity").css("margin-top", -30 + jQuery("#edit-field-activity").height() / -2);
-		jQuery("#edit-field-post").css("margin-top", -30 + jQuery("#edit-field-post").height() / -2);
-		jQuery("#edit-field-astrological-sign").css("margin-top", -30 + jQuery("#edit-field-astrological-sign").height() / -2);
-		jQuery("#edit-field-interests").css("margin-top", -30 + jQuery("#edit-field-interests").height() / -2);
-		jQuery("#edit-field-occasion").css("margin-top", -30 + jQuery("#edit-field-occasion").height() / -2);
+            jQuery('html, body').animate({
+                scrollTop: now * hPrecent
+            }, 0);
 
+            $('body').scrollspy('refresh');
+        }
 
+        /*jQuery(window).scroll(function(){
+         var scro = jQuery(window).scrollTop();
+         alert(scro);
+         });*/
 
-
-		function resize() {
-			//alert(jQuery('#edit-field-sex').height());
-
-			var current = jQuery(window).scrollTop();
-			var total = jQuery(document).height();
-			var hPrecent = current / total;
-
-		
-			var h = jQuery(window).height() - 60 < 600 ? 600 : jQuery(window).height() - 60;
-			var pad = jQuery(window).height() * .20;
-			jQuery(".results").css("min-height", h);
-			jQuery("#magial-submit-ajax .form-wrapper").height(h);
-			var sex_h = h - 122;
-			jQuery("#edit-sex").height(sex_h);
+        resize();
+        jQuery(window).bind('resize', function () {
+            resize();
+        });
 
 
-			var now = jQuery(document).height();
+        jQuery("#slider-age").slider({
+            range: true,
+            min: 1,
+            max: 115,
+            values: [1, 115],
+            slide: function (event, ui) {
+                jQuery("input#edit-field-age-min").val(jQuery("#slider-age").slider("values", 0));
+                jQuery("input#edit-field-age-max").val(jQuery("#slider-age").slider("values", 1));
+            },
+            stop: function (event, ui) {
+                jQuery("input#edit-field-age-min").val(jQuery("#slider-age").slider("values", 0));
+                jQuery("input#edit-field-age-max").val(jQuery("#slider-age").slider("values", 1));
+            }
+        });
 
-			jQuery('html, body').animate({
-				scrollTop: now * hPrecent,
-			}, 0);	
-		}
+        jQuery("#slider-price").slider({
+            range: true,
+            min: 1,
+            max: 103,
+            values: [1, 103],
+            slide: function (event, ui) {
+                price1 = nonLinear(jQuery("#slider-price").slider("values", 0));
+                price2 = nonLinear(jQuery("#slider-price").slider("values", 1));
+                jQuery("input#edit-field-price-min").val(price1);
+                jQuery("input#edit-field-price-max").val(price2);
+            },
+            stop: function (event, ui) {
+                price1 = nonLinear(jQuery("#slider-price").slider("values", 0));
+                price2 = nonLinear(jQuery("#slider-price").slider("values", 1));
+                jQuery("input#edit-field-price-min").val(price1);
+                jQuery("input#edit-field-price-max").val(price2);
+            }
 
-		/*jQuery(window).scroll(function(){
-		    var scro = jQuery(window).scrollTop();
-		    alert(scro);
-		});*/
+        });
 
-		resize();
-		jQuery(window).bind('resize', function(){
-			resize();
-		});
+        jQuery("#slider-relationship").slider({
+            min: 0,
+            max: 2,
+            value: 1,
+            slide: function (event, ui) {
+                //jQuery("#edit-field-relationship-" + ui.value).click();
+            }
 
+        });
 
-		jQuery("#slider-age").slider({
-			range: true,
-			min: 1,
-			max: 115,
-			values: [1,115],
-		    slide: function(event, ui){
-				 jQuery("input#edit-field-age-min").val(jQuery("#slider-age").slider("values",0));
-				 jQuery("input#edit-field-age-max").val(jQuery("#slider-age").slider("values",1));
-		    },
-		    stop: function(event, ui){
-		      jQuery("input#edit-field-age-min").val(jQuery("#slider-age").slider("values",0));
-		     jQuery("input#edit-field-age-max").val(jQuery("#slider-age").slider("values",1));
-		   }
-		});
+        /*function refresh_share_block()
+         {
 
-		jQuery("#slider-price").slider({
-			range: true,
-			min: 1,
-			max: 103,
-			values: [1,103],
-		    slide: function(event, ui){
-		    	price1 = nonLinear(jQuery("#slider-price").slider("values",0));
-		    	price2 = nonLinear(jQuery("#slider-price").slider("values",1));
-				jQuery("input#edit-field-price-min").val(price1);
-				jQuery("input#edit-field-price-max").val(price2);
-		    },
-		    stop: function(event, ui){
-		      price1 = nonLinear(jQuery("#slider-price").slider("values",0));
-		      price2 = nonLinear(jQuery("#slider-price").slider("values",1));
-		    jQuery("input#edit-field-price-min").val(price1);
-		    jQuery("input#edit-field-price-max").val(price2);
-		    }
+         }*/
 
-		});
+        function nonLinear(value) {
+            var price = value;
 
-		jQuery("#slider-relationship").slider({
-			min: 0,
-			max: 2,
-			value: 1,
-		    slide: function(event, ui){
-		    	jQuery("#edit-field-relationship-" + ui.value).click();
-		    	jQuery("#edit-field-relationship label").removeClass("checked");
-				jQuery('#label-edit-field-relationship-' + ui.value).toggleClass("checked");
-		    }
+            if (value < 26) {
+                price *= 100;
+            }
+            else if (value < 61) {
+                price -= 25;
+                price *= 500;
+                price += 2500;
+            }
+            else if (value < 81) {
+                price -= 60;
+                price *= 1000;
+                price += 20000;
+            }
+            else if (value < 91) {
+                price -= 80;
+                price *= 5000;
+                price += 40000;
+            }
+            else if (value < 100) {
+                price -= 90;
+                price *= 100000;
+                price += 0;
+            }
+            else
+                price = 1000000;
 
-		});
+            if (value <= 100)
+                return number_format(price, {decimals: 0, thousands_sep: "."}) + " р";
 
-		/*function refresh_share_block()
-		{
+            if (value == 101)
+                return "10.000.000€";
+            else if (value == 102)
+                return "100.000.000€";
+            else
+                return "300.000.000€";
+        }
 
-		}*/
+        function number_format(_number, _cfg) {
+            function obj_merge(obj_first, obj_second) {
+                var obj_return = {};
+                for (key in obj_first) {
+                    if (typeof obj_second[key] !== 'undefined') obj_return[key] = obj_second[key];
+                    else obj_return[key] = obj_first[key];
+                }
+                return obj_return;
+            }
 
-		function nonLinear(value) {
-			var price = value;
+            function thousands_sep(_num, _sep) {
+                if (_num.length <= 3) return _num;
+                var _count = _num.length;
+                var _num_parser = '';
+                var _count_digits = 0;
+                for (var _p = (_count - 1); _p >= 0; _p--) {
+                    var _num_digit = _num.substr(_p, 1);
+                    if (_count_digits % 3 == 0 && _count_digits != 0 && !isNaN(parseFloat(_num_digit))) _num_parser = _sep + _num_parser;
+                    _num_parser = _num_digit + _num_parser;
+                    _count_digits++;
+                }
+                return _num_parser;
+            }
 
-			if (value < 26)
-	    	{
-	    		price *= 100;	    		
-	    	}
-	    	else if (value < 61)
-	    	{
-	    		price -= 25;
-	    		price *= 500;
-	    		price += 2500;
-	    	}
-	    	else if (value < 81)
-	    	{
-	    		price -= 60;
-	    		price *= 1000;
-	    		price += 20000;
-	    	}
-	    	else if (value < 91)
-	    	{
-	    		price -= 80;
-	    		price *= 5000;
-	    		price += 40000;
-	    	}
-	    	else if (value < 100)
-	    	{
-	    		price -= 90;
-	    		price *= 100000;
-	    		price += 0;
-	    	}
-	    	else
-	    		price = 1000000;
+            if (typeof _number !== 'number') {
+                _number = parseFloat(_number);
+                if (isNaN(_number)) return false;
+            }
+            var _cfg_default = {before: '', after: '', decimals: 2, dec_point: '.', thousands_sep: ','};
+            if (_cfg && typeof _cfg === 'object') {
+                _cfg = obj_merge(_cfg_default, _cfg);
+            }
+            else _cfg = _cfg_default;
+            _number = _number.toFixed(_cfg.decimals);
+            if (_number.indexOf('.') != -1) {
+                var _number_arr = _number.split('.');
+                var _number = thousands_sep(_number_arr[0], _cfg.thousands_sep) + _cfg.dec_point + _number_arr[1];
+            }
+            else var _number = thousands_sep(_number, _cfg.thousands_sep);
+            return _cfg.before + _number + _cfg.after;
+        }
 
-	    	if (value <= 100)
-				return number_format(price, {decimals: 0, thousands_sep: "."}) + " р";
-
-			if (value == 101)
-				return "10.000.000€";
-			else if (value == 102)
-				return "100.000.000€";
-			else 
-				return "300.000.000€";
-		}
-
-		function number_format(_number, _cfg){
-		  function obj_merge(obj_first, obj_second){
-		    var obj_return = {};
-		    for (key in obj_first){
-		      if (typeof obj_second[key] !== 'undefined') obj_return[key] = obj_second[key];
-		      else obj_return[key] = obj_first[key];
-		      }
-		    return obj_return;
-		  }
-		  function thousands_sep(_num, _sep){
-		    if (_num.length <= 3) return _num;
-		    var _count = _num.length;
-		    var _num_parser = '';
-		    var _count_digits = 0;
-		    for (var _p = (_count - 1); _p >= 0; _p--){
-		      var _num_digit = _num.substr(_p, 1);
-		      if (_count_digits % 3 == 0 && _count_digits != 0 && !isNaN(parseFloat(_num_digit))) _num_parser = _sep + _num_parser;
-		      _num_parser = _num_digit + _num_parser;
-		      _count_digits++;
-		      }
-		    return _num_parser;
-		  }
-		  if (typeof _number !== 'number'){
-		    _number = parseFloat(_number);
-		    if (isNaN(_number)) return false;
-		  }
-		  var _cfg_default = {before: '', after: '', decimals: 2, dec_point: '.', thousands_sep: ','};
-		  if (_cfg && typeof _cfg === 'object'){
-		    _cfg = obj_merge(_cfg_default, _cfg);
-		  }
-		  else _cfg = _cfg_default;
-		  _number = _number.toFixed(_cfg.decimals);
-		  if(_number.indexOf('.') != -1){
-		    var _number_arr = _number.split('.');
-		    var _number = thousands_sep(_number_arr[0], _cfg.thousands_sep) + _cfg.dec_point + _number_arr[1];
-		  }
-		  else var _number = thousands_sep(_number, _cfg.thousands_sep);
-		  return _cfg.before + _number + _cfg.after;
-		}
-
-		jQuery("#secondary-menu a, a.anchor").slideto({
-		  speed: 800
-		});
-
-		jQuery("#secondary-menu a").click(function () {
-		    jQuery("#secondary-menu li").removeClass("active");
-		    jQuery(this).parent().addClass("active");
-		    /*new Ya.share({
-        		element: 'yashare'
-        	});*/
-    	});
-
-    	jQuery("a.anchor").click(function () {
-    		jQuery("#secondary-menu li").removeClass("active");
-    		jQuery('#secondary-menu a[href="' + jQuery(this).attr('href') + '"]').parent().addClass("active");
-    	});
+//        jQuery("#secondary-menu a").click(function () {
+//            jQuery("#secondary-menu li").removeClass("active");
+//            jQuery(this).parent().addClass("active");
+//            /*new Ya.share({
+//             element: 'yashare'
+//             });*/
+//        });
+//
+//        jQuery("a.anchor").click(function () {
+//            jQuery("#secondary-menu li").removeClass("active");
+//            jQuery('#secondary-menu a[href="' + jQuery(this).attr('href') + '"]').parent().addClass("active");
+//        });
 
 
-
-	});
+    });
 })(jQuery, Drupal, this, this.document);
 
 
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -77,16 +77,16 @@
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+        <?php if (!empty($site_name)): ?>
+            <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+        <?php endif; ?>
 
-        <nav id="secondary-menu" role="navigation">
+        <nav id="secondary-menu" class="pull-right navbar-scrollspy">
           <ul class="nav links inline clearfix">
               <li class="active"><a title="Пол" href="#top"></a></li>
               <li><a title="Возраст" href="#edit-age"></a></li>
@@ -109,8 +109,8 @@
       </button>-->
     </div>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <!--<div class="navbar-collapse collapse">
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+          <!--<div class="navbar-collapse collapse">
         <nav role="navigation">
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
@@ -123,57 +123,65 @@
           <?php endif; ?>
         </nav>
       </div>-->
-    <?php endif; ?>
+      <?php endif; ?>
   </div>
 </header>
+
+<?php if (!empty($page['jumbotron'])): ?>
+<div class="jumbotron">
+    <?php print render($page['jumbotron']); ?>
+</div>
+<?php endif; ?>
 
 <div class="main-container container">
 
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
+        <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
 
-    <?php print render($page['header']); ?>
+      <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
+
+
 
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
+        <aside class="col-sm-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
 
-    <section<?php print $content_column_class; ?>>
+      <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+          <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
+          <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+              <h1 class="page-header"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php if (!empty($tabs)): ?>
+              <?php print render($tabs); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['help'])): ?>
+              <?php print render($page['help']); ?>
+          <?php endif; ?>
+          <?php if (!empty($action_links)): ?>
+              <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?>
     </section>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <?php if (!empty($page['sidebar_second'])): ?>
+          <aside class="col-sm-3" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
+      <?php endif; ?>
 
   </div>
 </div>
